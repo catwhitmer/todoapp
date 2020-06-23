bfa9775fd311c38974ed50415d5e904d024bfcd3
catwhitmer <catwhitmer@gmail.com>
2020-06-23 12:38:00 -0400

    added state, click event, and strike through styling

--- src/components/Item.js
+++ src/components/Item.js
@@ -1,11 +1,24 @@
-import React from 'react'
+import React, { Component } from 'react'
+import { render } from '@testing-library/react';
 
 
-function Item(props) {
-  return (
-      <li>{props.desc}</li>
-  )
+class Item extends Component {
+  
+  state = {
+    done: false
+  }
 
+  handleClick = () => {
+    this.setState({done: !this.state.done })
+  }
+  
+  //add item controlled form
+  
+  render() {
+     return (
+      <li onClick = {this.handleClick} className={this.state.done ? "done" : null}>{this.props.desc}</li>
+     )
+  }
 }
 
 export default Item;
