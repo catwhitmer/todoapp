7d072bc1a91643cbefac4773f900b675e2620006
catwhitmer <catwhitmer@gmail.com>
2020-09-28 09:56:20 -0400

    coverted Todo to use hooks and added a fetch request

--- src/components/ToDo.js
+++ src/components/ToDo.js
@@ -1,22 +1,29 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import Item from './Item';
 import CreateTodo from './CreateTodo';
 
-class ToDo extends React.Component {
 
-  state= {
-      items: ['call mom', 'get groceries', 'do labs']
-  }
+function ToDo() {
 
-  render() {
-      return (
-        <ul>
-        <h2>My To-Dos</h2>
-          {this.state.items.map(item => <Item item= {item} />)}
-          <CreateTodo /> 
-        </ul>
-      )
-  }
+  const [ items, setItems ] = useState([]);
+
+  useEffect(() => {
+    fetch('http://localhost:3001/api/v1/todos')
+    .then (resp => resp.json())
+    .then ((data) => {
+      setItems(data.items)
+    })
+  }, [])
+
+
+  return (
+    <ul>
+      <h2> My To-Dos </h2>
+        
+        <CreateTodo /> 
+    </ul>
+  )
+
 }
 
 export default ToDo;
