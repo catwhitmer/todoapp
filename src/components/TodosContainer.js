d346ff49d44a5c92b963c5748286bbaee229496f
catwhitmer <catwhitmer@gmail.com>
2020-10-05 09:22:11 -0400

    changed the ToDos file to the TodosContainer, added axios, and rendered the todos

---
+++ src/components/TodosContainer.js
@@ -0,0 +1,39 @@
+import React, { Component } from 'react';
+//import Item from './Item';
+//import CreateTodo from './CreateTodo';
+import axios from 'axios';
+
+
+class TodosContainer extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      data: []
+    }
+  }
+  
+  componentDidMount() {
+     axios.get('http://localhost:3001/api/v1/todos')
+      .then (response => {
+        console.log(response)
+        this.setState({data: response.data})
+      })
+      .catch(error => console.log(error))
+  }
+
+  render() {
+    return (
+      <div>
+        {this.state.data.map((todo) => {
+          return(
+            <div className='Todos-Container' key={todo.id}>
+              <li> {todo.notes}</li>
+            </div> 
+          )
+        })}
+      </div>
+    )
+  }
+}
+
+export default TodosContainer;
