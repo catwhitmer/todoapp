4ed50651d094b6ad7a00327029ce5c146d8af267
catwhitmer <catwhitmer@gmail.com>
2020-10-26 09:18:47 -0400

    created the EditTodo component

---
+++ src/components/EditTodo.js
@@ -0,0 +1,15 @@
+import axios from 'axios'
+import React from 'react'
+
+class EditTodo extends React.Component {
+
+    render() {
+        return (
+            <div>
+            
+            </div>
+        )
+    }
+}
+
+export default EditTodo
