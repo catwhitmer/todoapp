ae3339a3c1d6a698c43933ec16fabeea85f77e06
catwhitmer <catwhitmer@gmail.com>
2020-11-09 08:14:44 -0500

    added prettier extension

--- src/components/CreateTodo.js
+++ src/components/CreateTodo.js
@@ -1,47 +1,51 @@
-import axios from 'axios'
-import React from 'react'
+import React from "react";
+import axios from "axios";
 
 class CreateTodo extends React.Component {
-
   state = {
-    notes: ''
-  }
+    notes: "",
+  };
 
-  handleChange = event => {
-    this.setState({ [event.target.name]: event.target.value })
-  }
+  handleChange = (event) => {
+    this.setState({ [event.target.name]: event.target.value });
+  };
 
-  handleSubmit = event => {
+  handleSubmit = (event) => {
     event.preventDefault();
 
     const todo = {
-      notes: this.state.notes
-    }
-     
+      notes: this.state.notes,
+    };
+
     axios
-      .post('http://localhost:3001/api/v1/todos', todo)
-      .then(resp => {
-      console.log(resp)
-      console.log(resp.data)
-      window.location = "/todos" 
-      this.setState({
-            notes: ''
-        })
-    })
-      .catch(error => console.log(error))
-  }
+      .post("http://localhost:3001/api/v1/todos", todo)
+      .then((resp) => {
+        console.log(resp);
+        console.log(resp.data);
+        window.location = "/todos";
+        this.setState({
+          notes: "",
+        });
+      })
+      .catch((error) => console.log(error));
+  };
 
   render() {
-    return(
+    return (
       <div>
-        <form className={'CreateTodo'} onSubmit={e => this.handleSubmit(e)}>
-          <label className={'TodoLabel'}>Add Todo</label>
-          <input type="text" name='notes' value={this.state.notes} onChange={this.handleChange} />
-          <input className={'SubmitButton'} type="submit" />
+        <form className={"CreateTodo"} onSubmit={(e) => this.handleSubmit(e)}>
+          <label className={"TodoLabel"}>Add Todo</label>
+          <input
+            type="text"
+            name="notes"
+            value={this.state.notes}
+            onChange={this.handleChange}
+          />
+          <input className={"SubmitButton"} type="submit" />
         </form>
       </div>
-    )
+    );
   }
 }
 
-export default CreateTodo
+export default CreateTodo;
